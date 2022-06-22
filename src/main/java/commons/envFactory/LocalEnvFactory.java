7ebd2647c6b2e25d9c01b11dc461797d27b2cf67
Nguyễn Ngọc Sơn <nnson0310@gmail.com>
2022-06-22 14:41:51 +0700

    Run test on docker grid

--- src/main/java/commons/envFactory/LocalEnvFactory.java
+++ src/main/java/commons/envFactory/LocalEnvFactory.java
@@ -1,17 +1,7 @@
 package commons.envFactory;
 
 import commons.browserFactory.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.CapabilityType;
-
-import java.util.Collections;
 
 public class LocalEnvFactory {
 
